3cb21c2291d2709db5074f43db41e0b5983c9293
Shivam Pandey <pandeyshivam2017robotics@gmail.com>
2022-05-05 01:31:10 +0530

    Dataloader testing scripts added.
    Signed-off-by: Shivam Pandey <pandeyshivam2017robotics@gmail.com>

---
+++ scripts/dl_test.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#SBATCH --job-name="Model-Verification"
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=3
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=128gb
+
+#SBATCH --error=job.clearn_train_v1.err
+#SBATCH --output=job.clearn_train_v1.out
+
+#SBATCH --time=01:00:00
+#SBATCH --partition=standard
+
+module load compiler/gcc/7.3.0 compiler/intel compiler/cuda compiler/cudnn
+module load python/conda-python/3.7 python/3.6
+module load cmake
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/apps/cdac/DL-CondaPy3.7/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/apps/cdac/DL-CondaPy3.7/etc/profile.d/conda.sh" ]; then
+        . "/home/apps/cdac/DL-CondaPy3.7/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/apps/cdac/DL-CondaPy3.7/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+conda activate ShivamPR21
+which python
+
+data_dir=$HOME/argoverse_dataset/train_1/argoverse-tracking/train1
+script_path=$HOME/research/CLORT/scripts
+
+out_dir=$HOME/research/clort_results
+mkdir -p $out_dir && cd $out_dir
+
+#python $HOME/collect_env.py
+python $script_path/dataloader_test.py $data_dir --batch_size 128 --n_epochs 10 --n_itr_logs 10 --log_id 0 --n_frames -1 --n_augs 5 \
+--vis_loss_w 0.7 --pcl_loss_w 0.7 --enc_loss_w 1.0 --vis_lr 0.0007 --pcl_lr 0.0007 --enc_lr 0.0003 \
+--itr_log_ln 10 --epoch_log_ln 1 --preload_model_path ''
