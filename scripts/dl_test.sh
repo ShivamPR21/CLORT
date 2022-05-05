0670a0bc4ccc762d274cada47146226d53777192
Shivam Pandey <pandeyshivam2017robotics@gmail.com>
2022-05-05 19:17:11 +0530

    scripts updated.
    Signed-off-by: Shivam Pandey <pandeyshivam2017robotics@gmail.com>

--- scripts/dl_test.sh
+++ scripts/dl_test.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name="Dataloader-testing@CLORT"
+#SBATCH --job-name="Dataloader-Testing@CLORT"
+#SBATCH --partition=standard
+#SBATCH --time=00:30:00
 
+### e.g. request 4 nodes with 1 gpu each, totally 4 gpus (WORLD_SIZE==4)
+### Note: --gres=gpu:x should equal to ntasks-per-node
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=3
 #SBATCH --cpus-per-task=15
 #SBATCH --mem=128gb
 
-#SBATCH --error=job.clearn_dl_test_v1.err
-#SBATCH --output=job.clearn_dl_test_v1.out
-
-#SBATCH --time=01:00:00
-#SBATCH --partition=standard
+#SBATCH --chdir=/scratch/pandey/
+#SBATCH --error=job.clort_model_train_verification_v1.err
+#SBATCH --output=job.clort_model_train_verification_v1.out
 
 module load compiler/gcc/7.3.0 compiler/intel compiler/cuda compiler/cudnn
 module load python/conda-python/3.7 python/3.6
@@ -41,6 +43,6 @@ out_dir=$HOME/research/clort_results
 mkdir -p $out_dir && cd $out_dir
 
 #python $HOME/collect_env.py
-python -m cProfile $script_path/dataloader_test.py $data_dir --batch_size 128 \
+srun python -m cProfile $script_path/dataloader_test.py $data_dir --batch_size 128 \
 --n_epochs 10 --n_itr_logs 10 --log_id 0 --n_frames -1 --n_augs 5 --vis_loss_w 0.7 --pcl_loss_w 0.7 --enc_loss_w 1.0 \
 --vis_lr 0.0007 --pcl_lr 0.0007 --enc_lr 0.0003 --itr_log_ln 10 --epoch_log_ln 1 --preload_model_path '' > $out_dir/dataloader_test.txt
