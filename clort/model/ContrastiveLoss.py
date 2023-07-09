4b82b80871725d17d9bc28c93e43170fc905450f
Shivam Pandey <pandeyshivam2017robotics@gmail.com>
2023-07-09 19:21:56 +0530

    Device syncing for ContrastiveLoss fixes in normalizing factor.
    Signed-off-by: Shivam Pandey <pandeyshivam2017robotics@gmail.com>

--- clort/model/ContrastiveLoss.py
+++ clort/model/ContrastiveLoss.py
@@ -238,7 +238,7 @@ class ContrastiveLoss(nn.Module):
             # Negative Counts for loss normalization
             n_cnt = np.concatenate(n_cnt).sum().reshape(-1, )
 
-        n_cnt = torch.tensor(n_cnt, dtype=torch.float32)
+        n_cnt = torch.tensor(n_cnt, dtype=torch.float32, device=x.device)
         _, temp_n = self._get_temp()
         ext_val = ((den - num.min()).detach()/temp_n).exp()
         loss_normalization_factor = ((Q+ext_val*n_cnt).log() - np.log(Q) + self.eps)/((1.+ext_val*n_cnt).log() + self.eps)
