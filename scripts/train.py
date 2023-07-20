a692b7acb4233a5dafef99acc11a697c411eeea9
Shivam Pandey <pandeyshivam2017robotics@gmail.com>
2023-07-20 21:58:06 +0530

    Temporary change in training script for model preload.
    Signed-off-by: Shivam Pandey <pandeyshivam2017robotics@gmail.com>

--- scripts/train.py
+++ scripts/train.py
@@ -294,8 +294,8 @@ def main(cfg: DictConfig):
     assert(run is not None and mb is not None and mb_infer is not None)
 
     if cfg.model.restore:
-        print("[Warning] : Prior Restore Temporary from id : r4722tp1")
-        ckpt = torch.load(run.restore(name="models/mdoel_10.pth", run_path="shivampr21/CLORTJSR/r4722tp1", replace=True).name)
+        print("[Warning] : Prior Restore Temporary from id : pbi1ca5m")
+        ckpt = torch.load(run.restore(name="models/model_20.pth", run_path="shivampr21/CLORTJSR/pbi1ca5m", replace=True).name)
         print(f'{enc.load_state_dict(ckpt["enc"], strict=False) = }') if cfg.restore.restore_model else print("Not restoring model parameters.")
 
         print(f'Loading model from file: {cfg.model.model_file = } \t {cfg.model.run_path = }')
